a36a7b1f66dcf5933f00f3c037fb6d2af997eab7
Guillermo Riaño <guilledark17@gmail.com>
2024-09-29 22:19:22 -0600

    ✨ New Feature: Fragment from React

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,17 +7,24 @@ function Card() {
       }}
     >
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <CardBody />
       </div>
     </div>
   );
 }
 
+export function CardBody() {
+  return (
+    <>
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+      <a href="#" className="btn btn-primary">
+        Go somewhere
+      </a>
+    </>
+  );
+}
 export default Card;
